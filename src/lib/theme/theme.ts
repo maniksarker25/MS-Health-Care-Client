d704fe64c18c67231d4def59a87b3f534d9652f2
Manik <tonysins43@gmail.com>
2024-04-30 14:45:23 +0600

    create theme for project

---
+++ src/lib/theme/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1586FD",
+    },
+    secondary: {
+      main: "#666f73",
+    },
+  },
+});
