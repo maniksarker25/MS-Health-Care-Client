8af512e82e959efdcd10b0d54bb0fa2e391365f7
Manik <tonysins43@gmail.com>
2024-05-01 16:42:43 +0600

    create how it works and stats section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,5 +1,7 @@
 import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
+import HowItWorks from "@/components/UI/HomePage/HowItWorks/HowItWorks";
 import Specialist from "@/components/UI/HomePage/Specialist/Specialist";
+import Stats from "@/components/UI/HomePage/Stats/Stats";
 import TopRatedDoctors from "@/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors";
 import WhyChooseUs from "@/components/UI/HomePage/WhyChooseUs/WhyChooseUs";
 
@@ -10,6 +12,8 @@ const HomePage = () => {
       <Specialist />
       <TopRatedDoctors />
       <WhyChooseUs />
+      <HowItWorks />
+      <Stats />
     </>
   );
 };
