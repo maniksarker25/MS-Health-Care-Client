90338f70ced02e4b8af7cce2c8b4b6631b8aed4f
Manik <tonysins43@gmail.com>
2024-05-04 12:41:01 +0600

    configure axios base query

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,8 +1,9 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
+import { createApi } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
-  reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+  reducerPath: "api",
+  baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
   endpoints: () => ({}),
 });
