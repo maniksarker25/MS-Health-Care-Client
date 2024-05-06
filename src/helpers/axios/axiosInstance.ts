b1bebf1bcae4fd6c93adf5ee1939fb53a68d9b30
Manik <tonysins43@gmail.com>
2024-05-06 20:19:20 +0600

    make axios instance

---
+++ src/helpers/axios/axiosInstance.ts
@@ -0,0 +1,41 @@
+import { authKey } from "@/constants/authKey";
+import { getFromLocalStorage } from "@/utils/localStorage";
+import axios from "axios";
+
+const instance = axios.create();
+
+instance.defaults.headers.post["Content-Type"] = "application/json";
+instance.defaults.headers["Accept"] = "application/json";
+instance.defaults.timeout = 60000;
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    const accessToken = getFromLocalStorage(authKey);
+    if (accessToken) {
+      config.headers.Authorization = accessToken;
+    }
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
+export { instance };
