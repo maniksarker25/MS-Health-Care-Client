b6efd77ff4d232b8a61e24be29c5b0fdda959cc4
Manik <tonysins43@gmail.com>
2024-05-12 18:16:49 +0600

    crate utis function for format date and time

--- src/app/(withDashboardLayout)/dashboard/admin/schedules/components/CreateScheduleModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/components/CreateScheduleModal.tsx
@@ -1,15 +1,21 @@
 import MSDatePicker from "@/components/Forms/MSDatePicker";
 import MSForm from "@/components/Forms/MSForm";
 import MSInput from "@/components/Forms/MSInput";
+import MSTimePicker from "@/components/Forms/MSTimePicker";
 import MSModal from "@/components/Shared/MSModal/MSModal";
 import { TMSModalProps } from "@/types/modal";
-import { Button, Grid } from "@mui/material";
+import { dateFormatter } from "@/utils/dateFormatter";
+import { timeFormatter } from "@/utils/timeFormatter";
+import { Box, Button, Grid } from "@mui/material";
 import React from "react";
 import { FieldValues } from "react-hook-form";
 
 const CreateScheduleModal = ({ open, setOpen }: TMSModalProps) => {
   const handleCreateSchedule = async (values: FieldValues) => {
-    console.log(values);
+    values.startDate = dateFormatter(values.startDate);
+    values.endDate = dateFormatter(values.endDate);
+    values.startTime = timeFormatter(values.startTime);
+    values.endTime = timeFormatter(values.endTime);
   };
   return (
     <MSModal open={open} setOpen={setOpen} title={"Create Schedule"}>
@@ -21,14 +27,29 @@ const CreateScheduleModal = ({ open, setOpen }: TMSModalProps) => {
             width: "400px",
           }}
         >
-          <Grid item md={12}>
+          <Grid item md={6}>
             <MSDatePicker name="startDate" label="Start Date" />
           </Grid>
-          <Grid item md={12}>
+          <Grid item md={6}>
             <MSDatePicker name="endDate" label="End Date" />
           </Grid>
+          <Grid item md={6}>
+            <MSTimePicker name="startTime" label="Start Time" />
+          </Grid>
+          <Grid item md={6}>
+            <MSTimePicker name="endTime" label="End Time" />
+          </Grid>
         </Grid>
-        <Button type="submit">Create</Button>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "end",
+          }}
+        >
+          <Button type="submit" sx={{ mt: "20px" }}>
+            Create
+          </Button>
+        </Box>
       </MSForm>
     </MSModal>
   );
